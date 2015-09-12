ce968eeb8d321ed85d5019abcdc69c8ba84ce1f7
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2015-09-12 14:55:12 +0300

    Data type for location

--- Celestial/Geo.hs
+++ Celestial/Geo.hs
@@ -1,3 +1,20 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
 -- |
 -- Module for working with points on the Earth
-module Celestial.Geo where
+module Celestial.Geo (
+    Location(..)
+  ) where
+
+import Data.Angle
+import Data.Data    (Typeable)
+import GHC.Generics (Generic)
+
+
+
+-- | Location on Earth
+data Location = Location
+  { geoLatitude  :: Angle Degrees Double
+  , getLongitude :: Angle Degrees Double
+  }
+  deriving (Show,Eq, Typeable,Generic)
